b9f4e6537e2084a6581b7a99a7477ce21f7e27c1
qihong.fu@yeepay.com <qihong.fu@yeepay.com>
2018-11-29 11:43:20 +0800

    dsada

---
+++ fea/config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+import sys
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def dropFrame(rows):
+    x = ''
+    for s in rows:
+        x += str(s) + ','
+    x = x[:-1]
+    return x
+
+
+
+nowTime=datetime.date.today()
+#yes_time = str(nowTime - datetime.timedelta(days=1))
+yes_time = '2018-11-02'
+input_mid_table_name = "dwd.fea_personal_cfsl_loan_deduct_seq_daily where dt <= '{yes_time}' and idcard is not null ".format(yes_time = yes_time)
+
+output_feature_hdfs_path = '/user/qihong.fu/test-feature-pool/fea_personal_cfsl_loan_daily/' + yes_time + '/'
---
+++ fea/dailyYpdata.sh
@@ -0,0 +1,48 @@
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py min.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py max.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py avg.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py sum_avg.py 
+
+
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_all.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_row_num.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_day_pay_1.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_day_pay_2.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_day_open_avg_1.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py count_day_open_avg_2.py 
+
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py third_count_1.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py third_count_2.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py third_count_3.py 
+spark-submit --master yarn-client --num-executors 15 --executor-memory 28G --executor-cores 10 --conf spark.default.parallelism=300 --py-files config.py third_2.py 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
